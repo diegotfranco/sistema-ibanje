0d380d6551526f6c9233402c65855051820f967a
Diego Franco <diegotfranco@gmail.com>
2025-10-13 23:55:57 -0400

    fix notfound route 2

--- backend/src/routes/notFound.ts
+++ backend/src/routes/notFound.ts
@@ -2,5 +2,5 @@ import type { Request, Response } from 'express';
 import { Errors } from '@/utils/errorFactory';
 
 export const notFound = (req: Request, _res: Response): void => {
-  throw Errors.notFound(`Rota não encontrada: ${req.method} - ${req.ip}${req.url}`);
+  throw Errors.notFound(`Rota não encontrada: ${req.method} - ${req.url}`);
 };
