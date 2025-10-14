300d04c0e020a98d0cf60fb0b4d5c25d45ddebc3
Diego Franco <diegotfranco@gmail.com>
2025-10-14 00:30:47 -0400

    removing secure cookies till prod has https

--- backend/src/db/postgres.ts
+++ backend/src/db/postgres.ts
@@ -1,16 +1,18 @@
 import postgres from 'postgres';
 import 'dotenv/config';
 
-if (!process.env.DB_HOST || !process.env.DB_USER || !process.env.DB_PASS || !process.env.DB_NAME) {
+const { DB_HOST, DB_USER, DB_PASS, DB_NAME } = process.env;
+
+if (!DB_HOST || !DB_USER || !DB_PASS || !DB_NAME) {
   throw new Error('Database enviroment variables not set');
 }
 
 export const sql = postgres({
-  host: process.env.DB_HOST,
+  host: DB_HOST,
   port: 5432,
-  username: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
+  username: DB_USER,
+  password: DB_PASS,
+  database: DB_NAME,
   idle_timeout: 20,
   max_lifetime: 60 * 30
 });
